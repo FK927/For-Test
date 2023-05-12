434d663d4df762944bd2e0337002c6f5641639a9
f-kon <f-kon@ceres-inc.jp>
2023-05-12 19:44:24 +0900

    ;追記

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <?php echo '100万円クイズハンター'; ?>
     <?php echo 9999-3800; ?>
     <?php for($i=1; $i<=100; $i++){
-	echo '今回の数値は' .$i .'です。<br />'
+	echo '今回の数値は' .$i .'です。<br />';
    }>
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
   </body>
